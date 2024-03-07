e4dd817a0f8fd6a7634161b5c7e04c8be9a32d99
guterfps <ithayg@gmail.com>
2024-03-07 14:06:08 +0200

    return multiple greetings

--- hello/hello.go
+++ hello/hello.go
@@ -13,8 +13,11 @@ func main() {
     // the time, source file, and line number.
     log.SetPrefix("greetings: ")
     log.SetFlags(0)
+
+	names := []string{"Ahi", "Ah Yakar", "Avi"}
+
 	// Request a greeting message.
-	message, err := greetings.Hello("Ahi")
+	messages, err := greetings.Hellos(names)
 	// If an error was returned, print it to the console and
 	// exit the program.
 	if err != nil {
@@ -23,5 +26,5 @@ func main() {
 
 	// If no error was returned, print the returned message
 	// to the console.
-	fmt.Println(message)
+	fmt.Println(messages)
 }
