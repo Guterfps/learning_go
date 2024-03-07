54fc4571f389ba337cdbdf71153b8767e805f7b2
guterfps <ithayg@gmail.com>
2024-03-07 13:47:23 +0200

    return random greeting

--- hello/hello.go
+++ hello/hello.go
@@ -14,7 +14,7 @@ func main() {
     log.SetPrefix("greetings: ")
     log.SetFlags(0)
 	// Request a greeting message.
-	message, err := greetings.Hello("")
+	message, err := greetings.Hello("Ahi")
 	// If an error was returned, print it to the console and
 	// exit the program.
 	if err != nil {
